a3b687aab2e6917807e5b42dbda17128d7ded65d
AugustLind <august04lind@gmail.com>
2025-02-20 21:27:10 +0100

    Queue system with errors

--- lab_2-0.1.0(1)/lab_2-0.1.0/skeleton_project/source/driver/stateMachine.c
+++ lab_2-0.1.0(1)/lab_2-0.1.0/skeleton_project/source/driver/stateMachine.c
@@ -1,127 +1,105 @@
 #include "stateMachine.h"
 #include "elevio.h"
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> // for sleep()
 
 void getToFirstFloor(){
-    //kjører ned til første etasje
     elevio_motorDirection(DIRN_DOWN);
-    while(elevio_floorSensor() != 0){
-        //venter til vi er i første etasje
+    while(elevio_floorSensor() != 0) {
+    
     }
     elevio_motorDirection(DIRN_STOP);
     elevio_doorOpenLamp(1);
-    //åpner døra
-    //venter i 3 sekunder
-    //lukker døra
+    sleep(3); 
     elevio_doorOpenLamp(0);
 }
 
-void init(){
-    //vite hvilken etasje
-    //tomme eventuell liste av bestillinger
-    //kanskje sjekk
-    //gjøre seg klar til bestillinger/arbeid
-    elevio_init();
+void init(struct StateMachine *state){
     getToFirstFloor();
+    state->currentFloor = elevio_floorSensor();
+    state->direction = DIRN_STOP;
     printf("Elevator is in first floor\n");
 }
 
-void getOrders(struct StateMachine *state){
-    //tar imot bestillinger
-    //sjekker om bestillingen er gyldig
-    //legger til bestillingen i køen
-    //sorterer køen
+void initQueue(struct StateMachine *state) {
+    for (int i = 0; i < MAX_ORDERS; i++){
+        state->queue[i] = -1;
+    }
+    state->orderCount = 0;
+}
 
-    //ytterløkke for knappetyper
-    //innerløkke for etasjer
-    for (ButtonType i = BUTTON_HALL_UP; i <= BUTTON_CAB; i++){
-        for (int j = 0; j < N_FLOORS; j++){
-            int index = j + i*4;
-            if (elevio_callButton(j, i)){
-                // printf("%d\n", elevio_callButton(j, i)); // Skriv ut heltallet som resultat av funksjonen
-                // printf("%d\n", j); 
-                if (index < 15){
-                    state->queue[index] = j;
-                }
-                else {
-                    printf("Queue is full\n");
-                }
-            }
-            else {
-                state->queue[index] = -1;
+void addOrder(struct StateMachine *state, int floor) {
+    for (int i = 0; i < state->orderCount; i++){
+        if (state->queue[i] == floor)
+            return;
+    }
+    if (state->orderCount < MAX_ORDERS){
+        state->queue[state->orderCount] = floor;
+        state->orderCount++;
+        printf("La til bestilling for etasje %d\n", floor);
+    } else {
+        printf("Køen er full\n");
+    }
+}
+
+void removeOrder(struct StateMachine *state, int floor) {
+    int index = -1;
+    for (int i = 0; i < state->orderCount; i++){
+        if (state->queue[i] == floor){
+            index = i;
+            break;
+        }
+    }
+    if (index != -1){
+        for (int i = index; i < state->orderCount - 1; i++){
+            state->queue[i] = state->queue[i+1];
+        }
+        state->orderCount--;
+        state->queue[state->orderCount] = -1;
+        printf("Fjernet bestilling for etasje %d\n", floor);
+    }
+}
+
+int getNextOrder(struct StateMachine *state) {
+    if (state->orderCount == 0)
+        return state->currentFloor;
+    int nextFloor = state->queue[0];
+    return nextFloor;
+}
+
+void getOrders(struct StateMachine *state) {
+    for (ButtonType btn = BUTTON_HALL_UP; btn <= BUTTON_CAB; btn++){
+        for (int floor = 0; floor < N_FLOORS; floor++){
+            if (elevio_callButton(floor, btn)) {
+                addOrder(state, floor);
             }
         }
     }
-
-}
-    // for (int i = 0; i < 15; i++){
-    //      printf("%d" , state->orders[i]);
-         
-    // }
-void nextFloor(struct StateMachine *state) {
-    //henter første verdi i køen 
-    int next = 2;
-    //sjekker hvilken etasje man er i og sammenligner
-    //om man skal lengre opp eller lengre ned 
-    int current = elevio_floorSensor();
-    // for (int i = 0; i < 15; i++){
-    //       printf("%d" , state->queue[i]);
-         
-    // }
-    // printf("/n");
-    //printf("%d",elevio_floorSensor());
-    if (current < next) {
-        state->direction = DIRN_UP;
-    }
-    if (current > next) {
-        state->direction = DIRN_DOWN;
-    }
-    if (current == next) {
-        state->direction = DIRN_STOP;
-    }
-    elevio_motorDirection(state->direction);
-    printf("%d", elevio_obstruction());
-    //åpne dør 
-}
-
-int compare (const void * a, const void * b){
-    return ( *(int*)a - *(int*)b );
 }
 
 void openDoor() {
-    //åpner og lukker døren 
-    //sjekk at man står i ro
-    //hvis stoppknapp: dør åpen så elnge man holder inne +3 sek 
-    int check = 1;
-    //sjekk at man ikke er mellom etasjer
-    while(elevio_obstruction() == 1) {
-        //antar at 0 er ingen og at 1 er obstruction
-        check = 0;
-    }
-    if (elevio_floorSensor() == -1) {
-        //mellom etasjer
-        check = 0;
-    }
-    if (check == 1) { //hvis alt er good
-
-    }
-    
+    elevio_motorDirection(DIRN_STOP);
+    elevio_doorOpenLamp(1);
+    sleep(3);
+    elevio_doorOpenLamp(0);
 }
 
-void sortQueue(struct StateMachine *state){
-    int index = 0;
-    
-    for (int i = 0; i < 15; i++){
-        if (state->queue[i] != -1){
-            state->orders[index++] = state->queue[i];
-        }    
-    }
-    qsort(state->orders, index, sizeof(int),compare);
+void nextFloor(struct StateMachine *state) {
+    int next = getNextOrder(state);
+    int current = elevio_floorSensor();
+    state->currentFloor = current;
 
-    for (int i = 0; i < 15; i++){
-          printf("%d" , state->orders[i]);
-         
+    if (current < next) {
+        state->direction = DIRN_UP;
+    } else if (current > next) {
+        state->direction = DIRN_DOWN;
+    } else {
+        state->direction = DIRN_STOP;
+        openDoor();
+        removeOrder(state, next);
+        return;
     }
+    elevio_motorDirection(state->direction);
+    printf("Heisen beveger seg %s mot etasje %d\n", (state->direction == DIRN_UP) ? "opp" : "ned", next);
 }
--- lab_2-0.1.0(1)/lab_2-0.1.0/skeleton_project/source/driver/stateMachine.h
+++ lab_2-0.1.0(1)/lab_2-0.1.0/skeleton_project/source/driver/stateMachine.h
@@ -1,19 +1,25 @@
 #pragma once
 #include <stdlib.h>
+#include "elevio.h"
+
+#define MAX_ORDERS 15
 
 struct StateMachine {
     int currentFloor;
     int nextFloor;
     int direction;
-    int active; //om den kjører eller ikke
-    int queue[15]; //kan ikke inisialisere en dynamisk array i struct
-    int *orders;
+    int active;          // om heisen er i bevegelse
+    int queue[MAX_ORDERS]; // lagrer etasjene med bestillinger
+    int orderCount;      // antall bestillinger i køen
 };
 
 void getToFirstFloor();
-void init();
-void nextFloor(struct StateMachine *state);
+void init(struct StateMachine *state);
 void getOrders(struct StateMachine *state);
-void sortQueue(struct StateMachine *state);
+void nextFloor(struct StateMachine *state);
+void openDoor();
 
-
+void initQueue(struct StateMachine *state);
+void addOrder(struct StateMachine *state, int floor);
+void removeOrder(struct StateMachine *state, int floor);
+int getNextOrder(struct StateMachine *state);
